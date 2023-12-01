eb2c39429bc054744994d722bdcfe8018819c619
noleemits <stevenoleemits@gmail.com>
2023-12-01 10:39:41 -0600

    Added feature to create a custom folder for the uploaded files

--- includes/functions.php
+++ includes/functions.php
@@ -100,3 +100,60 @@ function display_category_with_posts($category_id, $is_parent = false) {
 
     return $output;
 }
+
+//Change default directory for the file upload
+// Target 'file_upload' field for custom upload directory and filename
+add_filter('acf/upload_prefilter/name=file_upload', 'my_acf_upload_prefilter');
+function my_acf_upload_prefilter($errors) {
+    // File upload folder
+    add_filter('upload_dir', 'file_upload_folder', 20);
+    
+    // File upload name
+    add_filter('wp_handle_upload_prefilter', 'file_upload_name', 20);
+
+    return $errors;
+}
+
+// File upload folder - it creates it it doesn't exist
+function file_upload_folder($uploads) {
+    // Define the custom folder path
+    $custom_dir = '/customer_files';
+
+    // Full path to the custom directory
+    $custom_path = $uploads['basedir'] . $custom_dir;
+
+    // Check if the custom directory exists, and if not, create it
+    if (!file_exists($custom_path)) {
+        wp_mkdir_p($custom_path);
+    }
+
+    // Set the custom folder for uploads
+    $uploads['path'] = $custom_path;
+    $uploads['url'] = $uploads['baseurl'] . $custom_dir;
+    return $uploads;
+}
+
+
+// File upload name
+function file_upload_name($file) {
+    $file['name'] = "{$file['name']}";
+    return $file;
+}
+
+// Clean up thumbnails for PDFs after upload
+add_filter('wp_generate_attachment_metadata', 'cleanup_pdf_thumbnails', 10, 2);
+function cleanup_pdf_thumbnails($metadata, $attachment_id) {
+    $attachment = get_post($attachment_id);
+    if ($attachment->post_mime_type === 'application/pdf') {
+        $upload_dir = wp_upload_dir();
+        foreach ($metadata['sizes'] as $size => $fileinfo) {
+            $filepath = $upload_dir['path'] . '/' . $fileinfo['file'];
+            if (file_exists($filepath)) {
+                unlink($filepath);
+            }
+        }
+        $metadata['sizes'] = []; // Reset sizes to prevent database bloat
+    }
+    return $metadata;
+}
+
