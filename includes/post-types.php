1912953e41142e6f438f52e920811925c04d07e9
noleemits <45466598+noleemits@users.noreply.github.com>
2023-12-08 15:13:06 -0600

    Added post types to stop depending on ACF

---
+++ includes/post-types.php
@@ -0,0 +1,32 @@
+<?php
+
+// Register Custom Post Type Document
+function create_document_post_type() {
+    $args = array(
+        'labels' => array(
+            'name' => 'Documents',
+            'singular_name' => 'Document'
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'menu_icon' => 'dashicons-media-document',
+    );
+    register_post_type('document', $args);
+}
+
+// Register Custom Taxonomy
+function create_document_category_taxonomy() {
+    $args = array(
+        'labels' => array(
+            'name' => 'Folder',
+            'singular_name' => 'Folders'
+        ),
+        'public' => true,
+        'hierarchical' => true,
+    );
+    register_taxonomy('document_category', array('document'), $args);
+}
+
+add_action('init', 'create_document_post_type');
+add_action('init', 'create_document_category_taxonomy');
